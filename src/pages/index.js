6bda607d3c28dcfe19f7a6cd1af95349a888f0b4
PeterPavlatos <pavlatos.peter@gmail.com>
2023-05-21 09:58:25 -0400

    1 file modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,36 @@ import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [38.907132, -77.036546];
 
 export default function Home() {
   return (
     <Layout>
       <Head>
         <title>Next.js Leaflet Starter</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
+        <meta
+          name="description"
+          content="Create mapping apps with Next.js Leaflet Starter"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Section>
         <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
+          <h1 className={styles.title}>Next.js Leaflet Starter</h1>
 
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
+          <Map
+            className={styles.homeMap}
+            width="800"
+            height="400"
+            center={DEFAULT_CENTER}
+            zoom={12}
+          >
             {({ TileLayer, Marker, Popup }) => (
               <>
                 <TileLayer
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 <Marker position={DEFAULT_CENTER}>
                   <Popup>
@@ -42,14 +49,19 @@ export default function Home() {
           </Map>
 
           <p className={styles.description}>
-            <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
+            <code className={styles.code}>
+              yarn create next-app -e
+              https://github.com/colbyfayock/next-leaflet-starter
+            </code>
           </p>
 
           <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
+            <Button href="https://github.com/colbyfayock/next-leaflet-starter">
+              Vew on GitHub
+            </Button>
           </p>
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
